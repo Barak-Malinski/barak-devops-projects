749cd4ec1cadd4f469a78220f53723e0bf418cc9
Barak-Malinski <116379304+Barak-Malinski@users.noreply.github.com>
2026-06-16 18:48:03 +0300

    Update app.js

--- githubaction/aap.js
+++ githubaction/aap.js
@@ -7,7 +7,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Lets build the infrastructure of Jenkins and GitOps 0806 \n');
+  res.end('my name is barak the king \n');
 });
 
 server.listen(port, hostname, () => {
