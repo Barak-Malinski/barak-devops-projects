63eae2ae584f43cf0367780e87933de0c7175f34
Barak-Malinski <116379304+Barak-Malinski@users.noreply.github.com>
2026-06-22 12:59:14 +0300

    Update aap.js

--- githubaction/aap.js
+++ githubaction/aap.js
@@ -7,7 +7,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('my name is barak the king today \n');
+  res.end('my name is barak the king today 2\n');
 });
 
 server.listen(port, hostname, () => {
